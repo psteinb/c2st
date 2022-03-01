313e89d52d36b8922f3ae5dd5511ebe92e96adf1
Steve Schmerler <git@elcorto.com>
2022-03-01 22:20:39 +0100

    Add new test_c2st.py with test_api

--- tests/test_c2st.py
+++ tests/test_c2st.py
@@ -1,5 +1,29 @@
-from c2st import __version__
+import numpy as np
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import StratifiedKFold, KFold
+
+from c2st.check import c2st
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+rng = np.random.default_rng(seed=123)
+
+
+def randn(*size):
+    return rng.normal(loc=0, scale=1, size=size)
+
+
+def test_api():
+    X = randn(1000, 3)
+    Y = randn(100, 3)
+
+    ms = c2st(X, Y)
+    assert ms.ndim == 1
+    assert len(ms) == 1
+
+    ms = c2st(X, Y, clf=KNeighborsClassifier(5))
+    ms = c2st(X, Y, cv=StratifiedKFold())
+
+    ms, s = c2st(X, Y, return_scores=True, cv=KFold(7))
+    assert len(s) == 7
