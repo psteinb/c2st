135e340c19a40777a77132a4f077b888ddc728bb
Steve Schmerler <git@elcorto.com>
2022-03-01 18:07:50 +0100

    Accept more diverse classifiers, add passing CV splitter
    Classifier: Change API from passing a class (clf_class) + constructor
    kwds (clf_kwargs) to passing a class *instance*, e.g.
    RandomForestClassifier(random_state=1). Call this keyword "clf". This
    solves: Before we were passing random_state to clf_class but not all
    classifiers accept that, e.g. KNeighborsClassifier doesn't, which is the
    main motivation for this change.
    
    Add support for passing a CV splitter instance "cv", default is
    cv=KFold(n_splits=5, shuffle=True, random_state=1).
    
    Remove seed and n_folds. The seed (=random_state passed as int) must be
    specified by the user when passing cv and for some clf instances.

--- c2st/check.py
+++ c2st/check.py
@@ -8,50 +8,54 @@ from sklearn.model_selection import KFold, cross_val_score
 def c2st(
     X: np.ndarray,
     Y: np.ndarray,
-    seed: int = 1,
-    n_folds: int = 5,
     scoring: str = "balanced_accuracy",
     z_score: bool = True,
     noise_scale: Optional[float] = None,
     verbosity: int = 0,
-    clf_class=RandomForestClassifier,
-    clf_kwargs={},
+    clf=RandomForestClassifier(random_state=1),
+    cv=KFold(n_splits=5, shuffle=True, random_state=1),
     return_scores: bool = False,
 ) -> np.ndarray:
     """
-    Return accuracy of classifier trained to distinguish samples from supposedly
-    two distributions <X> and <Y>. For details on the method, see [1,2].
-    If the returned accuracy is 0.5, <X> and <Y> are considered to be from the
-    same generating PDF, i.e. they can not be differentiated.
-    If the returned accuracy is around 1., <X> and <Y> are considered to be from
-    two different generating PDFs.
+    Return accuracy of classifier trained to distinguish samples from
+    supposedly two distributions <X> and <Y>. For details on the method, see
+    [1,2]. If the returned accuracy is 0.5, <X> and <Y> are considered to be
+    from the same generating PDF, i.e. they can not be differentiated. If the
+    returned accuracy is around 1., <X> and <Y> are considered to be from two
+    different generating PDFs.
 
-    Trains classifiers with N-fold cross-validation [3]. By default, a `RandomForestClassifier`
-    by scikit-learn is used. This can be adopted using <clf_class> and
-    <clf_kwargs> as in:
+    Trains classifiers with N-fold cross-validation [3]. By default, a
+    `RandomForestClassifier` from scikit-learn is used. This can be changed by
+    passing another classifier instance, e.g.
 
-    ``` py
-    clf = clf_class(random_state=seed, **clf_kwargs)
-    #...
-    scores = cross_val_score(
-        clf, data, target, cv=shuffle, scoring=scoring, verbose=verbosity
-    )
-    ```
+    ::
+
+        c2st(..., clf=KNeighborsClassifier(5))
+
+    Also other CV methods can be specified that way.
+
+    ::
+
+        c2st(..., cv=StratifiedKFold(5))
 
     Args:
-        X: Samples from one distribution.
-        Y: Samples from another distribution.
+        X: Samples from one distribution, shape (n_samples_X, n_features)
+        Y: Samples from another distribution, shape (n_samples_Y, n_features)
         seed: Seed for sklearn
         n_folds: Number of folds
         z_score: Z-scoring using X
-        noise_scale: If passed, will add Gaussian noise with std noise_scale to samples of X and of Y
-        verbosity: control the verbosity of sklearn.model_selection.cross_val_score
-        clf_class: a scikit-learn classifier class
-        clf_kwargs: key-value arguments dictuinary to the class specified by clf_class, e.g. sklearn.ensemble.RandomForestClassifier
+        noise_scale: If passed, will add Gaussian noise with std noise_scale to
+            samples of X and of Y
+        verbosity: control the verbosity of
+            sklearn.model_selection.cross_val_score
+        clf: a scikit-learn classifier class instance
+        cv: cross-validation class instance with sklearn API, e.g.
+            sklearn.model_selection.KFold
         return_scores: Return 1d array of CV scores in addition to their mean
 
     Returns:
-        mean_scores: Mean of the accuracy scores over the test sets from cross-validation.
+        mean_scores: Mean of the accuracy scores over the test sets from
+            cross-validation.
         scores: 1d array of CV scores. Only if return_scores is True.
 
     Example:
@@ -79,16 +83,13 @@ def c2st(
     # X = X.cpu().numpy()
     # Y = Y.cpu().numpy()
 
-    clf = clf_class(random_state=seed, **clf_kwargs)
-
     # prepare data
     data = np.concatenate((X, Y))
     # labels
     target = np.concatenate((np.zeros((X.shape[0],)), np.ones((Y.shape[0],))))
 
-    shuffle = KFold(n_splits=n_folds, shuffle=True, random_state=seed)
     scores = cross_val_score(
-        clf, data, target, cv=shuffle, scoring=scoring, verbose=verbosity
+        clf, data, target, cv=cv, scoring=scoring, verbose=verbosity
     )
 
     mean_scores = np.atleast_1d(np.mean(scores).astype(np.float32))
