f7adbf6909b131df03a728025f92ba25a6f117f1
Steve Schmerler <git@elcorto.com>
2022-03-05 20:23:26 +0100

    Add c2st_ API test
    Also make test arrays smaller for faster tests.

--- tests/test_c2st.py
+++ tests/test_c2st.py
@@ -1,10 +1,9 @@
 import numpy as np
 
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import StratifiedKFold, KFold
 
-from c2st.check import c2st
+from c2st.check import c2st, c2st_
 
 
 rng = np.random.default_rng(seed=123)
@@ -15,11 +14,19 @@ def randn(*size):
 
 
 def test_api():
-    X = randn(1000, 3)
-    Y = randn(100, 3)
+    X = randn(10, 3)
+    Y = randn(5, 3)
 
     ms = c2st(X, Y, clf=KNeighborsClassifier(5))
     ms = c2st(X, Y, cv=StratifiedKFold())
 
     ms, s = c2st(X, Y, return_scores=True, cv=KFold(7))
+    assert len(s) == 7
+
+
+def test_api_underscore():
+    X = randn(10, 3)
+    Y = randn(5, 3)
+
+    s = c2st_(X, Y, cv=KFold(7))
     assert len(s) == 7
