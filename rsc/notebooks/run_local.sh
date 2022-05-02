5920d1244aef432ff9c6fe526a093ad6fea50302
Peter Steinbach <p.steinbach@hzdr.de>
2022-05-02 15:34:01 +0200

    generate AND run notebook

--- rsc/notebooks/run_local.sh
+++ rsc/notebooks/run_local.sh
@@ -3,15 +3,17 @@
 # nb=hyper.ipynb
 # py=hyper.py
 
+dst = ${1//.py/.ipynb}
+
 # Ensure clean notebook. Purge and (re-)create. Only input cells now.
-rm -f $2
+rm -fv ${dst}
 poetry run jupytext --to notebook $1
 
 # Pair if needed
 ##jupytext --set-formats ipynb,py:percent $nb
 
 # Run all cells, save outputs
-##jupytext $nb --execute
+poetry run jupytext $dst --execute
 
 # The same as `jupytext --execute` w/o jupytext
 ##jupyter nbconvert --to=notebook --inplace --ExecutePreprocessor.enabled=True $nb
