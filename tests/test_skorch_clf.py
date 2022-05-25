7572cea050c30eab3380d4aa8a0505fbf58d0330
Steve Schmerler <git@elcorto.com>
2022-05-25 15:07:54 +0200

    test: exec test_skorch_clf only if deps are found
    Do importlib gymnastics skip this test if needed.

--- tests/test_skorch_clf.py
+++ tests/test_skorch_clf.py
@@ -1,13 +1,25 @@
+import importlib
+
 import pytest
-
 import numpy as np
-from c2st.classifiers.skorch import skorch_classifier, skorch_binary_classifier
+
+##from c2st.classifiers.skorch import skorch_classifier, skorch_binary_classifier
 
 
+have_torch = importlib.util.find_spec("torch") is not None
+have_skorch = importlib.util.find_spec("skorch") is not None
+
+
+@pytest.mark.skipif(not have_torch, reason="torch not found")
+@pytest.mark.skipif(not have_skorch, reason="skorch not found")
 @pytest.mark.parametrize(
-    "build_clf", [skorch_classifier, skorch_binary_classifier]
+    "build_clf_name", ["skorch_classifier", "skorch_binary_classifier"]
 )
-def test_skorch_clf(build_clf):
+def test_skorch_clf(build_clf_name):
+    c2st_classifiers_skorch = importlib.import_module(
+        "c2st.classifiers.skorch"
+    )
+    build_clf = getattr(c2st_classifiers_skorch, build_clf_name)
     ndim = 3
     clf = build_clf(
         module__ndim_in=ndim,
