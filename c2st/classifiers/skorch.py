3a80061c046c9ccea5556cce05e2dbcf133f895a
Steve Schmerler <git@elcorto.com>
2022-08-10 18:34:13 +0200

    Improve docs in skorch support layer

--- c2st/classifiers/skorch.py
+++ c2st/classifiers/skorch.py
@@ -1,5 +1,143 @@
 """
-sklearn.neural_network.MLPRegressor-like API using skorch as NN backend.
+sklearn.neural_network.MLPClassifier-like API using skorch as NN backend,
+defining sensible defaults and shortcut functions for creating a classifier.
+
+
+Passing args and keywords
+=========================
+
+With skorch, we define a Module which is the NN model representing our
+classifier. This gets wrapped by skorch's NeuralNet{Binary}Classifier.
+The pre-defined modules we implement here are called
+
+SkorchClassifierModule          -> for NeuralNetClassifier
+SkorchBinaryClassifierModule    -> for NeuralNetBinaryClassifier
+
+Many settings like optimizer, lr, batch_size can be passes to
+NeuralNet{Binary}Classifier directly. Other kwds for the wrapped module must be
+passed as module__some_kwd=foo, which will do
+Skorch{Binary}ClassifierModule(..., some_kwd=foo). This is important in
+particular for custom kwds like hidden_layer_sizes in our case here.
+
+    clf = NeuralNetClassifier(
+        SkorchClassifierModule,
+        optimizer=AdamW,
+        lr=1e-3,
+        module__hidden_layer_sizes=(100,),
+        module__batch_norm=False,
+        ...
+        )
+    clf.fit(X_train, y_train)
+    clf.predict(X_test)
+
+We have shortcut functions for the above with sensible defaults that match
+sklearn's MLPClassifier as much as possible (see next section).
+
+    clf = skorch_classifier()
+    clf.fit(X_train, y_train)
+    clf.predict(X_test)
+
+
+Differences to sklearn
+======================
+
+default_kwds (see in code below): Defaults matching most of
+sklearn.neural_network.MLPClassifier's defaults, except
+
+* batch_size: sklearn uses min(200, n_samples), we'd need to define a custom
+  fit() to support setting this at runtime since we don't know n_samples
+  until fit() gets data passed in
+* max_iter is called max_epochs and our default is higher
+* weight decay: sklearn uses alpha > 0, we use Adam(weight_decay=0), so no
+  weight_decay. Reason: sklearn says "The L2 regularization term is divided
+  by the sample size when added to the loss.". Looking at the code, "sample
+  size" = batch_size but again it would be a bit of work to support the same
+  behavior using skorch's API. We'd need to set weight_decay=alpha/batch_size
+  at runtime once we know batch_size, not impossible but not our main concern
+  ATM either. So instead of choosing a different non-zero default, we disable
+  it. Also we actually recommend using AdamW over Adam if you want
+  weight_decay, so use
+
+    skorch_*_classifier(optimizer=AdamW,
+                        optimizer__weight_decay=1e-4/batch_size)
+  or
+
+    skorch_*_classifier(optimizer=Adam,
+                        optimizer__weight_decay=1e-4/batch_size)
+
+  to match sklearn's behavior.
+
+Equivalent param names:
+
+sklearn                   skorch
+-------                   ------
+learning_rate_init        lr
+n_iter_no_change          patience
+alpha                     Adam(weight_decay=alpha/batch_size)
+validation_fraction=0.1   ValidSplit(0.1)
+max_iter                  max_epochs      # for adam and sgd
+
+
+Loss functions used by NeuralNet{Binary}Classifier
+==================================================
+
+tl;dr in both cases, we effectively use cross-entropy loss.
+
+See https://github.com/skorch-dev/skorch/blob/master/skorch/toy.py:
+make_classifier() vs. make_binary_classifier(). In the general multi-class case
+we need to add softmax as last layer to output probabilities. From the skorch
+help:
+
+NeuralNetClassifier uses NLLLoss
+  criterion : torch criterion (class, default=torch.nn.NLLLoss)
+      Negative log likelihood loss. Note that the module should
+      return probabilities, the log is applied during get_loss.
+
+So the "module" (the NN model we define and wrap with NeuralNetClassifier())
+must output probabilities, created by a last softmax layer.
+
+"the log is applied in get_loss": From
+https://pytorch.org/docs/stable/generated/torch.nn.NLLLoss.html#torch.nn.NLLLoss:
+
+  The input given through a forward call is expected to contain
+  log-probabilities of each class.
+  ...
+  Obtaining log-probabilities in a neural network is easily achieved by
+  adding a LogSoftmax layer in the last layer of your network. You may
+  use CrossEntropyLoss instead, if you prefer not to add an extra
+  layer.
+
+So skorch replaces
+
+    NLLLoss(LogSoftmax(...))
+
+by something like
+
+    NLLLoss(get_loss(Softmax(...))
+
+Well ok. This means what we in effect use here is CrossEntropyLoss.
+
+NeuralNetBinaryClassifier uses BCEWithLogitsLoss
+  criterion : torch criterion (class, default=torch.nn.BCEWithLogitsLoss)
+      Binary cross entropy loss with logits. Note that the module should
+      return the logit of probabilities with shape (batch_size, ).
+
+From https://pytorch.org/docs/stable/generated/torch.nn.BCEWithLogitsLoss.html:
+    (BCE = binary cross entropy) This loss combines a Sigmoid layer and the
+    BCELoss in one single class. This version is more numerically stable than
+    using a plain Sigmoid followed by a BCELoss as, by combining the operations
+    into one layer, we take advantage of the log-sum-exp trick for numerical
+    stability.
+
+"logit of probabilities": In the binary case, the softmax layer can be
+rewritten to apply a sigmoid (=logistic function) to a single output instead of
+two (see Murphy 2022, book1, ch. 2.5.2). Since applying sigmoid(x) =
+1/(1+exp(-x)) to the last layer "produces probabilities" (well, clamps between
+[0,1]) and the logit function logit(x)=ln x/(1-x) is the inverse of sigmoid(x),
+the output of the last layer before applying sigmoid() is often called
+"logits". Therefore, we don't apply softmax to the last layer of the Module
+that we wrap with NeuralNetBinaryClassifier, meaning that "logit of
+probabilities" is just the last layer outputs themselves.
 """
 
 from collections import OrderedDict
@@ -14,6 +152,7 @@ from skorch.classifier import NeuralNetClassifier, NeuralNetBinaryClassifier
 class _SkorchClassifierModule(T.nn.Module):
     """Torch NN model for usage as skorch classifier module."""
 
+    # Bool, to be overwritten by derived classes.
     is_binary = None
 
     def __init__(
@@ -87,42 +226,6 @@ class SkorchBinaryClassifierModule(_SkorchClassifierModule):
     is_binary = True
 
 
-# Defaults matching most of sklearn.neural_network.MLPRegressor's defaults,
-# except:
-#
-# * batch_size: sklearn uses min(200, n_samples), we'd need to define a custom
-#   fit() to support setting this at runtime since we don't know n_samples
-#   until fit() gets data passed in
-# * max_iter
-# * weight decay: sklearn uses alpha > 0, we use Adam(weight_decay=0), so no
-#   weight_decay. Reason: sklearn says "The L2 regularization term is divided
-#   by the sample size when added to the loss.". Looking at the code, "sample
-#   size" = batch_size but again it would be a bit of work to support the same
-#   behavior using skorch's API. We'd need to set weight_decay=alpha/batch_size
-#   at runtime once we know batch_size, not impossible but not our main concern
-#   ATM either. So instead of choosing a different non-zero default, we disable
-#   it. Also we actually recommend using AdamW over Adam if you want
-#   weight_decay, so use
-#
-#     skorch_*_classifier(optimizer=AdamW,
-#                         optimizer__weight_decay=1e-4/batch_size)
-#   or
-#
-#     skorch_*_classifier(optimizer=Adam,
-#                         optimizer__weight_decay=1e-4/batch_size)
-#
-#   to match sklearn's behavior.
-#
-# Equivalent param names:
-#
-# sklearn                   skorch
-# -------                   ------
-# learning_rate_init        lr
-# n_iter_no_change          patience
-# alpha                     Adam(weight_decay=alpha/batch_size)
-# validation_fraction=0.1   ValidSplit(0.1)
-# max_iter                  max_epochs      # for adam and sgd
-#
 default_kwds = dict(
     max_epochs=10000,
     lr=1e-3,
@@ -144,7 +247,7 @@ default_kwds = dict(
     module__batch_norm=False,
 )
 # Adam(weight_decay=0) should be torch default anyway, but just to be safe;
-# note that AdamW's default weight_decay=1e-2
+# note that AdamW's default is weight_decay=1e-2
 default_kwds["optimizer__weight_decay"] = 0
 
 
