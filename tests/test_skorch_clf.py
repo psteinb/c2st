612e314f14c91be67f45eca398216dd6ce4144e1
Steve Schmerler <git@elcorto.com>
2022-09-05 22:41:19 +0200

    Cleanup test_skorch_clf

--- tests/test_skorch_clf.py
+++ tests/test_skorch_clf.py
@@ -4,6 +4,7 @@ import pytest
 import numpy as np
 
 from c2st.check import c2st
+
 ##from c2st.classifiers.skorch import skorch_classifier, skorch_binary_classifier
 
 
@@ -13,18 +14,14 @@ have_skorch = importlib.util.find_spec("skorch") is not None
 
 @pytest.mark.skipif(not have_torch, reason="torch not found")
 @pytest.mark.skipif(not have_skorch, reason="skorch not found")
-@pytest.mark.parametrize(
-    "build_clf_name", ["get_clf", "get_binary_clf"]
-)
-def test_skorch_clf(build_clf_name):
-    c2st_classifiers_skorch = importlib.import_module(
-        "c2st.classifiers.skorch"
-    )
-    build_clf = getattr(c2st_classifiers_skorch, build_clf_name)
+@pytest.mark.parametrize("get_clf_func", ["get_clf", "get_binary_clf"])
+def test_skorch_clf(get_clf_func):
+    import c2st.classifiers.skorch as c2st_classifiers_skorch
+
     ndim = 3
 
     def get_clf():
-        return build_clf(
+        return getattr(c2st_classifiers_skorch, get_clf_func)(
             module__ndim_in=ndim,
             module__hidden_layer_sizes=(20, 30),
             module__batch_norm=True,
